b17b133d4d0a1e437b9d4b551eefc7f2038e3c6b
tharsila <tharsilapaula@gmail.com>
2022-01-19 20:42:58 -0300

    add função nexIndex

--- mini projeto 2/script.js
+++ mini projeto 2/script.js
@@ -1,14 +1,31 @@
 const img = document.getElementById('img');
 const buttons = document.getElementById('buttons');
+let indexColors = 0;
 
 const turnOnLights = (event) => {
   turnON[event.target.id] ();
 }
 
+const nextIndex = () => {
+  if (indexColors < 2) {
+    indexColors++;
+  } else {
+    indexColors = 0;
+  } 
+}
+
+const changeColor = () => {
+  const colors = ['red','yellow','green'];
+  const color = colors[indexColors];
+  turnON[color] ();
+  nextIndex();
+}
+
 const turnON = {
-  'red':    () => img.src = 'img/vermelho.png',
-  'yellow': () => img.src = 'img/amarelo.png',
-  'green':  () => img.src = 'img/verde.png'
+  'red':       () => img.src = 'img/vermelho.png',
+  'yellow':    () => img.src = 'img/amarelo.png',
+  'green':     () => img.src = 'img/verde.png',
+  'automatic': () => setInterval(changeColor, 1000)
 }
 
 buttons.addEventListener('click', turnOnLights);
